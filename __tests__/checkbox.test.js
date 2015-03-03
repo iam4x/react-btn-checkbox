b5225c90c0423b57581dd2aa137eda12eb2dfe1f
Maxime Tyler <127086@supinfo.com>
2015-03-03 12:06:00 +0100

    :white_check_mark: add first test

--- __tests__/checkbox.test.js
+++ __tests__/checkbox.test.js
@@ -3,18 +3,45 @@
 'use strict';
 
 jest.dontMock('../src/checkbox');
+jest.dontMock('../src/mixins/render-buttons');
 
 describe('Checkbox', function () {
-
   it('should work', function () {
     var React = require('react/addons');
     var Checkbox = require('../src/checkbox');
     var TestUtils = React.addons.TestUtils;
 
+    var fakeCheckBox = {
+      'First': false,
+      'Second': true,
+      'Third': false
+    };
+
+    var handleChange = jest.genMockFunction();
+
     var component = TestUtils.renderIntoDocument(
-      <Checkbox />
+      <Checkbox
+        label={'Checkbox'}
+        options={fakeCheckBox}
+        onChange={handleChange} />
     );
 
-    expect(component.getDOMNode().textContent).toEqual('Checkbox');
+    expect(component.getDOMNode().children.length).toBe(2);
+
+    var labels = TestUtils.scryRenderedDOMComponentsWithTag(component, 'label');
+    // Label of button group
+    expect(labels[0].getDOMNode().textContent).toBe('Checkbox');
+    // Labels of buttons
+    expect(labels[1].getDOMNode().textContent).toBe('First');
+    expect(labels[2].getDOMNode().textContent).toBe('Second');
+    expect(labels[3].getDOMNode().textContent).toBe('Third');
+
+    // `Second` should be active
+    expect(labels[2].getDOMNode().className.indexOf('active')).not.toBe(-1);
+
+    // Simulate click
+    // check `handleChange` is called
+    TestUtils.Simulate.click(labels[1]);
+    expect(handleChange).toBeCalled();
   });
 });
